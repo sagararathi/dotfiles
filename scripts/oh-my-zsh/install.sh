300bc2fecca134589f3ab489f59edf1997105860
Justin Grubbs <justin@jgrubbs.net>
2017-03-15 23:32:35 -0400

    updates

--- scripts/oh-my-zsh/install.sh
+++ scripts/oh-my-zsh/install.sh
@@ -12,3 +12,8 @@ else
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
   report "oh-my-zsh installed successfully"
 fi
+
+if [ ! -d "$HOME/antigen.zsh" ]; then
+  curl https://cdn.rawgit.com/zsh-users/antigen/v1.4.1/bin/antigen.zsh > "$HOME/antigen.zsh"
+  source "$HOME/antigen.zsh"
+fi
