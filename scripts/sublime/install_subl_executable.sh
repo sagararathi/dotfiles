e480f1cb8c327e11e702da7942655765a0612fd2
Justin Grubbs <justin@kingandpartners.com>
2017-03-23 13:43:08 -0400

    install subl command in executables dir

---
+++ scripts/sublime/install_subl_executable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+. helper_scripts/print_format
+. helper_scripts/link_file
+
+link_file "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" "$DOTFILE_PATH/executables/subl"
